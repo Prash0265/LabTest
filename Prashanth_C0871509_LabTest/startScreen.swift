1726611838aaa7f4317e9aea843359213a46d26f
Prash0265 <kmrprashanth92@gmail.com>
2022-11-04 19:07:32 -0400

    message

---
+++ Prashanth_C0871509_LabTest/startScreen.swift
@@ -0,0 +1,84 @@
+//
+//  ViewController.swift
+//  Prashanth_C0871509_LabTest
+//
+//  Created by Others on 2022-11-04.
+//
+
+import UIKit
+
+class startScreen: UIViewController {
+
+    var counter = 00
+    var minsCounter = 00
+    var timer = Timer()
+    var isPlaying = false
+
+    @IBOutlet weak var timeLabel: UILabel!
+    @IBOutlet weak var lapButton: UIButton!
+    @IBOutlet weak var startButton: UIButton!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        //timeLabel.text = String(counter)
+        timeLabel.text = "00:00"
+        //stopButton.isEnabled = false
+        
+    }
+    
+    
+    @IBAction func startTimer(_ sender: Any) {
+        //timeLabel.text = "00:00"
+        let mainSB : UIStoryboard = UIStoryboard(name: "Main", bundle: nil)
+        let stopScreenVC = mainSB.instantiateViewController(withIdentifier: "stopScreen")
+        /// make a modal segue
+//            self.present(homeVC, animated: true, completion: nil)
+        navigationController?.pushViewController(stopScreenVC, animated: true)
+    }
+    //    @IBAction func startTimer(_ sender: AnyObject) {
+//        if(isPlaying) {
+//                return
+//            }
+//            startButton.isEnabled = false
+//            stopButton.isEnabled = true
+//
+//            timer = Timer.scheduledTimer(timeInterval: 1, target: self, selector: #selector(UpdateTimer), userInfo: nil, repeats: true)
+//            isPlaying = true
+//    }
+//    @IBAction func pauseTimer(_ sender: AnyObject) {
+//        startButton.isEnabled = true
+//           stopButton.isEnabled = false
+//
+//           timer.invalidate()
+//           isPlaying = false
+//    }
+//    @IBAction func resetTimer(_ sender: AnyObject) {
+//        startButton.isEnabled = true
+//           stopButton.isEnabled = false
+//
+//           timer.invalidate()
+//           isPlaying = false
+//        counter = 00
+//
+//        timeLabel.text = "\(minsCounter):\(counter)"
+//    }
+    
+    @objc func UpdateTimer() {
+        counter = counter + 01
+        if counter == 60
+        {
+            minsCounter = minsCounter + 01
+            counter = 00
+            timeLabel.text = "\(minsCounter):\(counter)"
+        }
+        timeLabel.text = "\(minsCounter):\(counter)"
+    }
+
+
+}
+
+  
+
+
+
+
---
+++ Prashanth_C0871509_LabTest/stopScreen.swift
@@ -0,0 +1,29 @@
+//
+//  stopScreen.swift
+//  Prashanth_C0871509_LabTest
+//
+//  Created by Others on 2022-11-04.
+//
+
+import UIKit
+
+class stopScreen: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
